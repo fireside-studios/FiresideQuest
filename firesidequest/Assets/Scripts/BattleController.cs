1ceb95e6b1a7427e92d2baaa45e1ec3aec1e11a8
PatKopec <patrick.kopec21@gmail.com>
2019-08-23 18:59:49 -0400

    Initialized folder structure. Created Main scene and BattleInstance scene. Added temporary sprites and background images. Created script to control battle logic. Added simple movement animation when attacking.

---
+++ firesidequest/Assets/Scripts/BattleController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform enemy;
+    public float speed = 1.0f;
+
+    private Vector3 startPos;
+    private float currentLerpTime;
+    private float returnLerpTime;
+    private bool isAttacking = false;
+    private bool isReturning = false;
+
+
+    void Start()
+    {
+        startPos = player.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space")) {
+            currentLerpTime = 0f;
+            isAttacking = true;
+        }
+
+        if (isAttacking)
+        {
+            AttackMovement(startPos, enemy.transform.position);
+        }
+
+        if (isReturning) {
+            returnLerpTime = 0f;
+            ReturnToStart(player.transform.position, startPos);
+        }
+        
+
+    }
+
+    void AttackMovement(Vector3 start, Vector3 target)
+    {
+
+        if (currentLerpTime > 1.0f)
+        {
+            currentLerpTime = 0f;
+            isAttacking = false;
+            isReturning = true;
+        }
+
+        if (isAttacking)
+        {
+            currentLerpTime += (Time.deltaTime);
+            float perc = currentLerpTime / 1.0f;
+            player.transform.position = Vector3.Lerp(start, target, perc);
+        }
+    }
+
+    void ReturnToStart(Vector3 start, Vector3 target)
+    {
+        if (returnLerpTime > 1.0f)
+        {
+            returnLerpTime = 1.0f;
+            isReturning = false;
+        }
+
+        if (isReturning)
+        {
+            returnLerpTime += (Time.deltaTime);
+            float perc = returnLerpTime / 1.0f;
+            player.transform.position = Vector3.Lerp(start, target, perc);
+        }
+    }
+
+}
