fd0fbb0f5931b2ec396e27c9bf4f2f24c384932c
PatKopec <patrick.kopec21@gmail.com>
2019-08-29 13:28:10 -0400

    added selectZone around characters as a precursor to choosing who attacks who using the mouse/UI

---
+++ firesidequest/Assets/Scripts/SelectZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectZone : MonoBehaviour
+{
+    public Transform selectZoneTransform;
+    public Color startColor;
+    public Color mouseOverColor;
+    bool mouseOver = false;
+    SpriteRenderer selectZoneSprite;
+    
+
+    private void Start()
+    {
+        mouseOver = true;
+        selectZoneSprite = transform.GetComponent<SpriteRenderer>();
+        selectZoneSprite.color = startColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        selectZoneSprite.color = mouseOverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        selectZoneSprite.color = startColor;
+    }
+
+}
